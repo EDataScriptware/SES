58a1c434c30c953bf148da4e212a01cec0ab4e72
Sam Sit <ss4521@rit.edu>
2021-07-24 09:21:54 -0400

    Update main.py
    Made it so that it only start sequence detection if v is pressed.

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ from pynput.keyboard import Listener
 from enum_lookups import action_checker
 
 key_sequence = []
+combo = False
 
 def on_press(key):
+    global combo
     print("Key pressed: {0}".format(key))
     concat_string = ""
-    key_sequence.append(key)
-    print(key_sequence)
+    if (combo == True):
+        key_sequence.append(key)
+        print(key_sequence)
+    if(format(key).strip("'").lower() == 'v' and (combo == False)):
+        combo = True
+        key_sequence.append(key)
+        print(key_sequence)
     if len(key_sequence) == 3:
+        combo = False
         for key_sequence_value in key_sequence:
             concat_string += str(key_sequence_value).strip("'").lower()
         try:
