1d1e9c0e90701e3dcd0cfe47e6f899082cb16f55
Edward Riley <55459510+EDataScriptware@users.noreply.github.com>
2021-08-03 18:36:11 -0400

    Adds phasmophobia functionality

--- discord_file.py
+++
@@ -1,7 +0,0 @@
-from discord_webhook import DiscordWebhook
-
-def send_message(message):
-    WEBHOOK_URL = "https://discord.com/api/webhooks/872195980425064458/hH3OxjytbYxIlV-oOiEX7eBujtppzBh_Tv6mrIO7jXL2tr-ei4-3--5RnVuLW2riYB2t"
-    webhook = DiscordWebhook(url=WEBHOOK_URL, content=str(message))
-    response = webhook.execute()
-    print(response)
