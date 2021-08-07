3e84ebb655a779b14a9ed23ffd900d9c8e0a9b6c
Sam Sit <ss4521@rit.edu>
2021-08-07 18:23:16 -0400

    Add menu bar
    Also gitingore add game commands json

--- GUI.py
+++ GUI.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import *
 from util.keystroke_listener import everything
 
 window = tk.Tk()
@@ -21,11 +22,49 @@ def on_entry_click(event):
         firstclick = False
         nameEntry.delete(0, "end") # delete all the text in the entry
 
+def donothing():
+   x = 0
 
+#menu bar
+menubar = Menu(window)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="New", command=donothing)
+filemenu.add_command(label="Open", command=donothing)
+filemenu.add_command(label="Save", command=donothing)
+filemenu.add_command(label="Export", command=donothing)
+#filemenu.add_separator()
+filemenu.add_command(label="Exit", command=window.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+
+editmenu = Menu(menubar, tearoff=0)
+editmenu.add_command(label="Redo", command=donothing)
+editmenu.add_command(label="Undo", command=donothing)
+editmenu.add_command(label="Delete all", command=donothing)
+editmenu.add_command(label="Reset to default", command=donothing)
+menubar.add_cascade(label="Edit", menu=editmenu)
+
+viewmenu = Menu(menubar, tearoff=0)
+viewmenu.add_command(label="Games", command=donothing)
+viewmenu.add_command(label="Hotkeys", command=donothing)
+menubar.add_cascade(label="View", menu=viewmenu)
+
+helpmenu = Menu(menubar, tearoff=0)
+helpmenu.add_command(label="Tutorial", command=donothing)
+menubar.add_cascade(label="Help", menu=helpmenu)
+
+aboutmenu = Menu(menubar, tearoff=0)
+aboutmenu.add_command(label="Upgrade", command=donothing)
+aboutmenu.add_command(label="Donate", command=donothing)
+aboutmenu.add_command(label="Contact us", command=donothing)
+menubar.add_cascade(label="About", menu=aboutmenu)
+
+window.config(menu=menubar)
+
+#name box
 nameEntry.bind('<FocusIn>', on_entry_click)
 nameEntry.grid(column=0, row=0)
 
-
+#buttons
 button_dead_by_daylight = tk.Button(
     text="Play Dead by Daylight",
     width=25,
---
+++ menu test.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+def donothing():
+    x = 0
+
+
